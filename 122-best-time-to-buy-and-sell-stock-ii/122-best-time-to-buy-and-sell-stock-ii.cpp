1459bb75d6b03b620ea91d85daf75c95f4f114f8
rini-810 <108979543+rini-810@users.noreply.github.com>
2022-08-07 00:08:02 +0530

    Time: 4 ms (92.57%), Space: 13 MB (89.51%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    //peak valley approach
+    //single pass method 
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        int valley=prices[0];
-        int peak=prices[0];
-        int maxprofit=0;
-        int i=0;
-       while(i<n-1){
-           while(i<n-1 && prices[i]>=prices[i+1])
-               i++;
-               valley=prices[i];
-           while(i<n-1 && prices[i]<=prices[i+1])
-               i++;
-               peak=prices[i];
-              
-            maxprofit += peak - valley;
-       }
-        return maxprofit;
+        int profit=0;
+      for(int i=1;i<n;i++){
+          if(prices[i]>prices[i-1]){
+              profit += prices[i]- prices[i-1];
+          }
+      }
+      return profit;
     }
 };
