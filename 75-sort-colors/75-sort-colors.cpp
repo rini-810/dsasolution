8a3a05f18b404eeec9d2606fe806116f03b9aa3a
rini-810 <108979543+rini-810@users.noreply.github.com>
2022-08-09 11:40:58 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (99.52%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int high=nums.size()-1;
+        int low=0;
+        int mid=0;
+        int temp=0;
+        
+        while(mid<=high){
+            if(nums[mid]==0){
+            temp= nums[low];
+                nums[low]=nums[mid];
+                nums[mid]= temp;
+                low++;
+                mid++;
+                }
+            else if(nums[mid]==1){
+               mid++;
+        
+            }
+            else{
+                temp= nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
     }
 };
