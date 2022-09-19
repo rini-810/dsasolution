8b1122bf1ed71046cf378a50d721a2dc4323e3fd
rini-810 <108979543+rini-810@users.noreply.github.com>
2022-09-19 13:55:03 +0530

    Time: 15 ms (24.24%), Space: 10.8 MB (20.19%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans;
         unordered_map<int,int> hm;
-        for(int i=0;i<nums1.size();i++){
-            hm[nums1[i]]++;
+        for(int i=0;i<nums2.size();i++){
+            hm[nums2[i]]++;
             
         }
-        for(int j=0;j<nums2.size();j++){
-            if(hm[nums2[j]]>0){
-                hm[nums2[j]]--;
-                ans.push_back(nums2[j]);
+        for(int j=0;j<nums1.size();j++){
+            if(hm[nums1[j]]>0){
+                hm[nums1[j]]--;
+                ans.push_back(nums1[j]);
             }
         }
         return ans;
