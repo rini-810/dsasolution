5820e0596bd910828b9b66d80a6202dfed53e402
rini-810 <108979543+rini-810@users.noreply.github.com>
2023-01-14 21:27:49 +0530

    Time: 3 ms (75.23%), Space: 7 MB (13.36%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,120 +1,32 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        return creat(a,b);
-    }
-  string creat(string a,string b)
-  {
-      string ans="";
-      int a_start=a.size()-1;
-      int b_start=b.size()-1;
-      int carry=0;
-      while(a_start>=0 && b_start>=0)
-      {
-          //cout<<"carry"<<carry;
-          if(a[a_start]=='1' && b[b_start]=='1')
-          {
-              if(carry==1)
-              {
-                  ans='1'+ans;
-                  
-              }
-              else
-              {
-              ans='0'+ans;
-              
-              }
-              carry=1;
-              
-          }
-         else if((a[a_start]=='1' && b[b_start]=='0')||(a[a_start]=='0' && b[b_start]=='1'))
-          {
-              if(carry==1)
-              {
-                  ans='0'+ans;
-                  carry=1;
-              }
-             else
-             {
-                 ans='1'+ans;
-                 //carry=0;
-             }
-          }
-          else if(a[a_start]=='0' && b[b_start]=='0')
-          {
-              if(carry==1)
-              {
-                  ans='1'+ans;
-                  carry=0;
-              }
-              else
-              {
-                  ans='0'+ans;
-              }
-          }
-          //cout<<ans;
-          a_start--;
-          b_start--;
-      }
-      while(a_start>=0)
-      {
-          if(a[a_start]=='1')
-          {
-              if(carry==1)
-              {
-                  ans='0'+ans;
-                  carry=1;
-              }
-              else
-              {
-                  ans='1'+ans;
-              }
-          }
-          else
-          {
-              if(carry==1){
-                  
-              ans='1'+ans;
-                  carry=0;
-              }
-              else{
-                  ans='0'+ans;
-              }
-          }
-          a_start--;
-          
-      }
-      while(b_start>=0)
-      {
-          if(b[b_start]=='1')
-          {
-              if(carry==1)
-              {
-                  ans='0'+ans;
-                  carry=1;
-              }
-              else
-              {
-                  ans='1'+ans;
-              }
-          }
-          else
-          {
-              if(carry==1){
-                  
-              ans='1'+ans;
-                  carry=0;
-              }
-              else{
-                  ans='0'+ans;
-              }
-          }
-          b_start--; 
-      }
-      if(carry==1)
-      {
-          ans='1'+ans;
-      }
-      return ans;
-  }
+        stack<string> binary;
+        int i=a.size()-1;
+        int j=b.size()-1;
+        int sum=0,carry=0;
+        string ans;
+        while(i>=0 || j>=0){
+              if(i<0)  sum=(b[j]-'0') + carry;
+              else if(j<0)   sum=(a[i]-'0')+ carry;
+             else  sum=(a[i]-'0')+(b[j]-'0') + carry;
+            
+            if(sum>=2)
+                sum==2 ? binary.push("0"),carry=1 : (binary.push("1"),carry=1);
+            if(sum==0)
+                binary.push("0"),carry=0;
+            if(sum==1) 
+                binary.push("1"),carry=0;
+            i--;
+            j--;
+            }
+        if(carry==1)
+            binary.push("1");
+        while(!binary.empty()){
+           ans += binary.top();
+            binary.pop();
+        }
+        return ans;
+        
+        }
 };
