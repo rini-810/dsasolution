44b1447d6632347811f440638843632ce10e480e
rini-810 <108979543+rini-810@users.noreply.github.com>
2023-01-24 14:43:21 +0530

    Time: 12 ms (97.58%), Space: 6.4 MB (79.07%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int knightDialer(int n) {
+        int mod=1e9+7;
+       long dp[n][10];
+        for(int i=0;i<10;i++){
+            dp[0][i]=1;
+        }
+        
+        for(int i=1;i<n;i++){
+            dp[i][0]= (dp[i-1][4]+dp[i-1][6])%mod;
+            
+            dp[i][1]=(dp[i-1][6]+dp[i-1][8])%mod;
+            
+            dp[i][2]=(dp[i-1][7]+dp[i-1][9])%mod;
+            
+            dp[i][3]=(dp[i-1][4]+dp[i-1][8])%mod;
+            
+            dp[i][4]=(dp[i-1][0]+dp[i-1][3]+ dp[i-1][9])%mod;
+            
+            dp[i][5]=0;
+            
+            dp[i][6]=(dp[i-1][0]+dp[i-1][1]+dp[i-1][7])%mod;
+            
+            dp[i][7]=(dp[i-1][2]+dp[i-1][6])%mod;
+            
+            dp[i][8]=(dp[i-1][1]+dp[i-1][3])%mod;
+            
+            dp[i][9]=(dp[i-1][2]+dp[i-1][4])%mod;
+        }
+        
+        long ans=0;
+        for(int i=0;i<10;i++){
+            ans =(ans+dp[n-1][i])%mod;
+        }
+        
+        return (int) ans;
+    }
+};
