a7eec17f4f7f9f2488345ef4fe7203552ae1393f
rini-810 <108979543+rini-810@users.noreply.github.com>
2023-03-12 23:25:02 +0530

    Time: 267 ms (14.33%), Space: 13.3 MB (58.02%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,28 +10,22 @@
  */
 class Solution {
 public:
-   struct compare {
-    bool operator()(const ListNode* l, const ListNode* r) {
-        return l->val > r->val;
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
     }
-};
-ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
-    priority_queue<ListNode *, vector<ListNode *>, compare> q;
-    for(auto l : lists) {
-        if(l)  q.push(l);
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
     }
-    if(q.empty())  return NULL;
-
-    ListNode* result = q.top();
-    q.pop();
-    if(result->next) q.push(result->next);
-    ListNode* tail = result;            
-    while(!q.empty()) {
-        tail->next = q.top();
-        q.pop();
-        tail = tail->next;
-        if(tail->next) q.push(tail->next);
-    }
-    return result;
-}
 };
