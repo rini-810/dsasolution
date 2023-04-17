f30ad499a7a7dd22ab6398ef7d079ecf6fc88c23
rini-810 <108979543+rini-810@users.noreply.github.com>
2023-04-17 23:17:17 +0530

    Time: 2 ms (71.33%), Space: 8.9 MB (64.81%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> ans;
+        auto m= *max_element(candies.begin(),candies.end());
+        
+        for(int i=0;i<candies.size();i++){
+            if(candies[i]+extraCandies>=m){
+                ans.push_back(true);
+            }
+            else
+                ans.push_back(false);
+        }
+        
+        return ans;
+    }
+};
