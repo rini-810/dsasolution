9fd984479311e0ac95bf219cd7794232d226bc86
rini-810 <108979543+rini-810@users.noreply.github.com>
2023-04-26 10:41:20 +0530

    Time: 3 ms (56.14%), Space: 5.9 MB (63.10%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+      return rec(num);
+        
+    }
+    int rec(int num){
+      int a= num/10;
+        int b=num%10;
+        if(num<10)  return num;
+        // if(a==0 && b>0 ){
+        //     return 1;
+        // }
+        
+        int c = a+b;
+        int ans=rec(c);
+        return ans;
+    }
+};
