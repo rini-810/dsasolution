7adf8ff169111f4d163fff02b06da27be0534983
rini-810 <108979543+rini-810@users.noreply.github.com>
2023-05-04 20:52:02 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (79.26%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> a={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+        vector<string> result;
+        if(digits.empty())
+            return result;
+        result.push_back("");
+        
+        for(auto d:digits){
+            vector<string> temp;
+            for(auto x:a[d-'0']){
+                for(auto s:result){
+                    temp.push_back(s+x);
+                }
+            }
+            swap(result,temp);
+        }
+        
+        return result;
+    }
+};
